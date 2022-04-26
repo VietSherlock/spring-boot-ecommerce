6a5e39966ffb903eca3dae7e1a86353e917053de
Viet Le Nguyen Ngoc <viet.lenguyenngoc@nttdata.com>
2022-04-26 07:18:43 +0700

    add Helper class

--- src/main/java/com/vietsherlock/ecommerce/utils/Helper.java
+++ src/main/java/com/vietsherlock/ecommerce/utils/Helper.java
@@ -2,7 +2,7 @@ package com.vietsherlock.ecommerce.utils;
 
 public class Helper {
 
-    private Helper(){}
+    private Helper(){}  //helper class should have private constructor
 
     public static boolean isNotNull(Object object){
         return object != null;
