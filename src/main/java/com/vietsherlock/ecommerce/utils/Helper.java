d1ac20356965d6c5a8e946acd0b78cc43f6c768d
Viet Le Nguyen Ngoc <viet.lenguyenngoc@nttdata.com>
2022-04-26 07:17:49 +0700

    add Helper class

---
+++ src/main/java/com/vietsherlock/ecommerce/utils/Helper.java
@@ -0,0 +1,11 @@
+package com.vietsherlock.ecommerce.utils;
+
+public class Helper {
+
+    private Helper(){}
+
+    public static boolean isNotNull(Object object){
+        return object != null;
+    }
+
+}
